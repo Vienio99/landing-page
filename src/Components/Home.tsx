db82695d0e6fcdf547c2b75c44ccef4e319bf7fa
Vienio99 <kyve96@gmail.com>
2022-02-08 23:22:01 +0100

    additional styling and html components

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -6,12 +6,12 @@ import "./styles/Home.css";
 function Home() {
   return (
     <div id="HomeContainer">
-      <header className="header">
+      <div className="headerWrapper">
         {/* Container for top items */}
-        <div className="wrapper">
+        <header className="header">
           <div className="navbarContainer">
             <h2>DeFi Dashboard</h2>
-            <div className="linksContainer">
+            <div className="container">
               <a>Overview</a>
               <a>Top DeFi Protocols</a>
               <a>Quote</a>
@@ -21,31 +21,80 @@ function Home() {
           <div className="pageDescContainer">
             {/* Left */}
             <div>
-              <h1>This website is </h1>
-              <h1>awesome</h1>
+              <p className="descTitle">This website is </p>
+              <p className="descTitle">awesome</p>
               <p>
                 This website has some subtext that goes under here under the
               </p>
               <p>main title. It's a smaller font and the color is lower </p>
               <p>contrast.</p>
-              <button className="signUpBtn">Sign up</button>
+              <button className="signUpBtnTop">Sign up</button>
             </div>
             {/* Right */}
             <div className="imageBox">
               <p>This is a placeholder for image</p>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
+      </div>
       <div className="nftSection">
         <h1>Some random information.</h1>
-        <div className="linksContainer">
+        <div className="container">
           <div className="nftContainer">
-            <img src="" />
-            <p>this is some subtext under an illustration or image</p>
+            <div className="nftImg" />
+            <p>this is some subtext</p>
+            <p>under an illustration</p>
+            <p> or image</p>
+          </div>
+          <div className="nftContainer">
+            <div className="nftImg" />
+            <p>this is some subtext</p>
+            <p>under an illustration</p>
+            <p> or image</p>
+          </div>
+          <div className="nftContainer">
+            <div className="nftImg" />
+            <p>this is some subtext</p>
+            <p>under an illustration</p>
+            <p> or image</p>
+          </div>
+          <div className="nftContainer">
+            <div className="nftImg" />
+            <p>this is some subtext</p>
+            <p>under an illustration</p>
+            <p> or image</p>
           </div>
         </div>
       </div>
+      <div className="quoteSectionWrapper">
+        <div className="quoteSection">
+          <div className="textContainer">
+            <em>
+              <q>
+                This is an inspiring quote, or a testimonial from a customer.
+                Maybe it's just filling up space, or maybe people will actually
+                read it. Who knows? All I know is that it looks nice.
+              </q>
+            </em>
+            <p className="quoteAuthor">-Thor, God of Thunder</p>
+          </div>
+        </div>
+      </div>
+      <div className="inviteSectionWrapper">
+        <div className="inviteSection">
+          <div className="inviteContainer">
+            <div className="inviteText">
+              <h1>Call to action! It's time!</h1>
+              <p>
+                Sign up for our product by clicking that button right over
+                there!
+              </p>
+            </div>
+            <button className="signUpBtnBtm">Sign up</button>
+          </div>
+        </div>
+      </div>
+      <footer>Copyright The Odin Project 2021</footer>
     </div>
   );
 }
